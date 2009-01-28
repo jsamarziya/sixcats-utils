1e8406ee1a60fd337d70b403ee1780510b428f9c
jsamarziya <jsamarziya@dd3cb808-b827-0410-b84f-d54cde0580db>
2009-01-28 02:09:14 +0000

    fixed warnings

--- sixcats-utils/src/main/java/org/sixcats/utils/CollectionUtils.java
+++ sixcats-utils/src/main/java/org/sixcats/utils/CollectionUtils.java
@@ -30,39 +30,40 @@ import org.apache.commons.lang.ObjectUtils;
 
 public class CollectionUtils {
     private static final Transformer GET_KEY_VALUE_KEY_TRANSFORMER = new Transformer() {
-	public Object transform(Object input) {
-	    return ((KeyValue) input).getKey();
-	}
+        public Object transform(Object input) {
+            return ((KeyValue<?, ?>) input).getKey();
+        }
     };
-    private static final Comparator KEY_VALUE_KEY_COMPARATOR = new TransformingComparator(
-	    GET_KEY_VALUE_KEY_TRANSFORMER);
+    @SuppressWarnings("unchecked")
+    private static final Comparator<Object> KEY_VALUE_KEY_COMPARATOR = new TransformingComparator(
+            GET_KEY_VALUE_KEY_TRANSFORMER);
 
     /**
      * Indexes a collection of objects by their string value.
      * 
      * @param elements
-     *                the objects to index
+     *            the objects to index
      * @return a Map containing the objects indexed into lists, which are keyed
      *         by the first character of their string value
      */
     public static <T> SortedMap<String, List<T>> indexedCollection(
-	    final Collection<T> elements) {
-	SortedMap<String, List<T>> retval = new TreeMap<String, List<T>>(
-		String.CASE_INSENSITIVE_ORDER);
-	for (T element : elements) {
-	    String key = null;
-	    String s = ObjectUtils.toString(element);
-	    if (s.length() > 0) {
-		key = s.substring(0, 1);
-	    }
-	    List<T> l = retval.get(key);
-	    if (l == null) {
-		l = new ArrayList<T>();
-		retval.put(key, l);
-	    }
-	    l.add(element);
-	}
-	return retval;
+            final Collection<T> elements) {
+        SortedMap<String, List<T>> retval = new TreeMap<String, List<T>>(
+                String.CASE_INSENSITIVE_ORDER);
+        for (T element : elements) {
+            String key = null;
+            String s = ObjectUtils.toString(element);
+            if (s.length() > 0) {
+                key = s.substring(0, 1);
+            }
+            List<T> l = retval.get(key);
+            if (l == null) {
+                l = new ArrayList<T>();
+                retval.put(key, l);
+            }
+            l.add(element);
+        }
+        return retval;
     }
 
     /**
@@ -73,24 +74,23 @@ public class CollectionUtils {
      * @return a mapping of calendar years to lists of calendar date/value
      *         pairs. Each list is sorted by date.
      */
-    @SuppressWarnings("unchecked")
     public static <V> SortedMap<Integer, List<KeyValue<CalendarDate, V>>> getIndexedDateCountMap(
-	    Map<CalendarDate, V> dateValueMap) {
-	final SortedMap<Integer, List<KeyValue<CalendarDate, V>>> retval = new TreeMap<Integer, List<KeyValue<CalendarDate, V>>>();
-	for (Map.Entry<CalendarDate, V> entry : dateValueMap.entrySet()) {
-	    final CalendarDate date = entry.getKey();
-	    final V value = entry.getValue();
-	    final Integer year = date.getYear();
-	    List<KeyValue<CalendarDate, V>> l = retval.get(year);
-	    if (l == null) {
-		l = new ArrayList<KeyValue<CalendarDate, V>>();
-		retval.put(year, l);
-	    }
-	    l.add(new DefaultKeyValue<CalendarDate, V>(date, value));
-	}
-	for (List<KeyValue<CalendarDate, V>> l : retval.values()) {
-	    Collections.sort(l, KEY_VALUE_KEY_COMPARATOR);
-	}
-	return retval;
+            Map<CalendarDate, V> dateValueMap) {
+        final SortedMap<Integer, List<KeyValue<CalendarDate, V>>> retval = new TreeMap<Integer, List<KeyValue<CalendarDate, V>>>();
+        for (Map.Entry<CalendarDate, V> entry : dateValueMap.entrySet()) {
+            final CalendarDate date = entry.getKey();
+            final V value = entry.getValue();
+            final Integer year = date.getYear();
+            List<KeyValue<CalendarDate, V>> l = retval.get(year);
+            if (l == null) {
+                l = new ArrayList<KeyValue<CalendarDate, V>>();
+                retval.put(year, l);
+            }
+            l.add(new DefaultKeyValue<CalendarDate, V>(date, value));
+        }
+        for (List<KeyValue<CalendarDate, V>> l : retval.values()) {
+            Collections.sort(l, KEY_VALUE_KEY_COMPARATOR);
+        }
+        return retval;
     }
 }
