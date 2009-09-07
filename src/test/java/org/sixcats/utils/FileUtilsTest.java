3d04a2ed294bcf614e97efbe24be566afe30674b
Jeffrey Samarziya <jsamarziya@users.noreply.github.com>
2009-09-07 05:54:04 +0000

    added FileUtils#makeTempDirectory()

--- src/test/java/org/sixcats/utils/FileUtilsTest.java
+++ src/test/java/org/sixcats/utils/FileUtilsTest.java
@@ -15,8 +15,11 @@
  */
 package org.sixcats.utils;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
@@ -27,54 +30,67 @@ import org.junit.Test;
 public class FileUtilsTest {
     @Test
     public void testIsAncestor1() throws IOException {
-	assertTrue(FileUtils.isAncestor(new File("/foo/bar"), new File(
-		"/foo/bar")));
+        assertTrue(FileUtils.isAncestor(new File("/foo/bar"), new File("/foo/bar")));
     }
 
     @Test
     public void testIsAncestor2() throws IOException {
-	assertTrue(FileUtils.isAncestor(new File("/foo"), new File("/foo/baz")));
+        assertTrue(FileUtils.isAncestor(new File("/foo"), new File("/foo/baz")));
     }
 
     @Test
     public void testIsAncestor3() throws IOException {
-	assertFalse(FileUtils.isAncestor(new File("/foo/bar"), new File(
-		"/bar/foo")));
+        assertFalse(FileUtils.isAncestor(new File("/foo/bar"), new File("/bar/foo")));
     }
 
     @Test
     public void testIsAncestor4() throws IOException {
-	assertTrue(FileUtils.isAncestor(new File("/foo/bar"), new File(
-		"/foo/bar/baz/faz/../../xyz")));
+        assertTrue(FileUtils.isAncestor(new File("/foo/bar"),
+                new File("/foo/bar/baz/faz/../../xyz")));
     }
 
     @Test
     public void testIsAncestor5() throws IOException {
-	assertFalse(FileUtils.isAncestor(new File("/foo/bar/baz"), new File(
-		"/foo/bar/baz/../../xyz")));
+        assertFalse(FileUtils.isAncestor(new File("/foo/bar/baz"), new File(
+                "/foo/bar/baz/../../xyz")));
     }
 
     @Test
     public void testIsAncestor6() throws IOException {
-	assertFalse(FileUtils.isAncestor(new File("/foo/bar/baz"), new File(
-		"/foo/bar/baz/../../../faz")));
+        assertFalse(FileUtils.isAncestor(new File("/foo/bar/baz"), new File(
+                "/foo/bar/baz/../../../faz")));
     }
 
     @Test
     public void testGetRelativePath1() throws IOException {
-	assertEquals("baz", FileUtils.getRelativePath(new File("/foo/bar"),
-		new File("/foo/bar/baz")));
+        assertEquals("baz", FileUtils.getRelativePath(new File("/foo/bar"),
+                new File("/foo/bar/baz")));
     }
 
     @Test
     public void testGetRelativePath2() throws IOException {
-	assertEquals("", FileUtils.getRelativePath(new File("/foo/bar"),
-		new File("/foo/bar")));
+        assertEquals("", FileUtils.getRelativePath(new File("/foo/bar"), new File("/foo/bar")));
     }
 
     @Test
     public void testGetRelativePath3() throws IOException {
-	assertEquals("baz" + File.separator + "faz", FileUtils.getRelativePath(
-		new File("/foo/bar"), new File("/foo/bar/baz/faz")));
+        assertEquals("baz" + File.separator + "faz", FileUtils.getRelativePath(
+                new File("/foo/bar"), new File("/foo/bar/baz/faz")));
+    }
+
+    @Test
+    public void testMakeTempDirectory() throws IOException {
+        final File tmp = FileUtils.makeTempDirectory("foo", null);
+        assertThat(tmp.exists(), is(true));
+        try {
+            assertThat(tmp.isDirectory(), is(true));
+            assertThat(tmp.list().length, is(0));
+            assertThat(tmp.getName().startsWith("foo"), is(true));
+            assertThat(tmp.getName().endsWith(".tmp"), is(true));
+            assertThat(tmp.getName(), is(not("foo.tmp")));
+        } finally {
+            org.apache.commons.io.FileUtils.forceDelete(tmp);
+        }
+
     }
 }
