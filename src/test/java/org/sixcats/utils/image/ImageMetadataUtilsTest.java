c9fc5fe52190b8af5f63cb00cfba46520be8e921
Jeffrey Samarziya <jsamarziya@users.noreply.github.com>
2010-01-13 00:50:43 +0000

    added getNormalizedSize(), isRotated() to ImageMetadataUtils

--- src/test/java/org/sixcats/utils/image/ImageMetadataUtilsTest.java
+++ src/test/java/org/sixcats/utils/image/ImageMetadataUtilsTest.java
@@ -64,18 +64,27 @@ public class ImageMetadataUtilsTest {
         size = ImageMetadataUtils.getSize(new File("target/test-classes/animclam.gif"));
         assertThat(size.width, is(120));
         assertThat(size.height, is(94));
+        size = ImageMetadataUtils.getSize(new File("target/test-classes/ariel.jpg"));
+        assertThat(size.width, is(3072));
+        assertThat(size.height, is(2304));
         size = ImageMetadataUtils.getSize(new File("target/test-classes/Daisies.bmp"));
         assertThat(size.width, is(157));
         assertThat(size.height, is(130));
         size = ImageMetadataUtils.getSize(new File("target/test-classes/DSC_0085.JPG"));
         assertThat(size.width, is(3008));
         assertThat(size.height, is(2000));
-        size = ImageMetadataUtils.getSize(new File("target/test-classes/IMG_0480.JPG"));
-        assertThat(size.width, is(3072));
-        assertThat(size.height, is(2304));
         size = ImageMetadataUtils.getSize(new File("target/test-classes/HATCH3.gif"));
         assertThat(size.width, is(175));
         assertThat(size.height, is(100));
+        size = ImageMetadataUtils.getSize(new File("target/test-classes/IMG_0210.JPG"));
+        assertThat(size.width, is(600));
+        assertThat(size.height, is(800));
+        size = ImageMetadataUtils.getSize(new File("target/test-classes/IMG_0480.JPG"));
+        assertThat(size.width, is(3072));
+        assertThat(size.height, is(2304));
+        size = ImageMetadataUtils.getSize(new File("target/test-classes/killyoukitty.jpg"));
+        assertThat(size.width, is(600));
+        assertThat(size.height, is(450));
         size = ImageMetadataUtils.getSize(new File("target/test-classes/PA080040.JPG"));
         assertThat(size.width, is(1280));
         assertThat(size.height, is(960));
@@ -88,16 +97,92 @@ public class ImageMetadataUtilsTest {
     }
 
     @Test
+    public void testGetNormalizedSize() throws IOException {
+        Dimension size;
+        size = ImageMetadataUtils.getNormalizedSize(new File("target/test-classes/animclam.gif"));
+        assertThat(size.width, is(120));
+        assertThat(size.height, is(94));
+        size = ImageMetadataUtils.getNormalizedSize(new File("target/test-classes/ariel.jpg"));
+        assertThat(size.width, is(2304));
+        assertThat(size.height, is(3072));
+        size = ImageMetadataUtils.getNormalizedSize(new File("target/test-classes/Daisies.bmp"));
+        assertThat(size.width, is(157));
+        assertThat(size.height, is(130));
+        size = ImageMetadataUtils.getNormalizedSize(new File("target/test-classes/DSC_0085.JPG"));
+        assertThat(size.width, is(3008));
+        assertThat(size.height, is(2000));
+        size = ImageMetadataUtils.getNormalizedSize(new File("target/test-classes/HATCH3.gif"));
+        assertThat(size.width, is(175));
+        assertThat(size.height, is(100));
+        size = ImageMetadataUtils.getNormalizedSize(new File("target/test-classes/IMG_0210.JPG"));
+        assertThat(size.width, is(600));
+        assertThat(size.height, is(800));
+        size = ImageMetadataUtils.getNormalizedSize(new File("target/test-classes/IMG_0480.JPG"));
+        assertThat(size.width, is(3072));
+        assertThat(size.height, is(2304));
+        size = ImageMetadataUtils
+                .getNormalizedSize(new File("target/test-classes/killyoukitty.jpg"));
+        assertThat(size.width, is(600));
+        assertThat(size.height, is(450));
+        size = ImageMetadataUtils.getNormalizedSize(new File("target/test-classes/PA080040.JPG"));
+        assertThat(size.width, is(1280));
+        assertThat(size.height, is(960));
+        size = ImageMetadataUtils.getNormalizedSize(new File("target/test-classes/project.png"));
+        assertThat(size.width, is(800));
+        assertThat(size.height, is(600));
+        size = ImageMetadataUtils.getNormalizedSize(new File("target/test-classes/ram.jpg"));
+        assertThat(size.width, is(533));
+        assertThat(size.height, is(400));
+    }
+
+    @Test
     public void testGetOrientation() throws IOException {
+        assertThat(ImageMetadataUtils.getOrientation(new File("target/test-classes/animclam.gif")),
+                is(1));
         assertThat(ImageMetadataUtils.getOrientation(new File("target/test-classes/ariel.jpg")),
                 is(6));
+        assertThat(ImageMetadataUtils.getOrientation(new File("target/test-classes/Daisies.bmp")),
+                is(1));
         assertThat(ImageMetadataUtils.getOrientation(new File("target/test-classes/DSC_0085.JPG")),
                 is(1));
-        assertThat(ImageMetadataUtils.getOrientation(new File("target/test-classes/ram.jpg")),
+        assertThat(ImageMetadataUtils.getOrientation(new File("target/test-classes/HATCH3.gif")),
+                is(1));
+        assertThat(ImageMetadataUtils.getOrientation(new File("target/test-classes/IMG_0210.JPG")),
+                is(1));
+        assertThat(ImageMetadataUtils.getOrientation(new File("target/test-classes/IMG_0480.JPG")),
                 is(1));
         assertThat(ImageMetadataUtils.getOrientation(new File(
                 "target/test-classes/killyoukitty.jpg")), is(1));
-        assertThat(ImageMetadataUtils.getOrientation(new File("target/test-classes/IMG_0210.JPG")),
+        assertThat(ImageMetadataUtils.getOrientation(new File("target/test-classes/PA080040.JPG")),
                 is(1));
+        assertThat(ImageMetadataUtils.getOrientation(new File("target/test-classes/project.png")),
+                is(1));
+        assertThat(ImageMetadataUtils.getOrientation(new File("target/test-classes/ram.jpg")),
+                is(1));
+    }
+
+    @Test
+    public void testIsRotated() throws IOException {
+        assertThat(ImageMetadataUtils.isRotated(new File("target/test-classes/animclam.gif")),
+                is(false));
+        assertThat(ImageMetadataUtils.isRotated(new File("target/test-classes/ariel.jpg")),
+                is(true));
+        assertThat(ImageMetadataUtils.isRotated(new File("target/test-classes/Daisies.bmp")),
+                is(false));
+        assertThat(ImageMetadataUtils.isRotated(new File("target/test-classes/DSC_0085.JPG")),
+                is(false));
+        assertThat(ImageMetadataUtils.isRotated(new File("target/test-classes/HATCH3.gif")),
+                is(false));
+        assertThat(ImageMetadataUtils.isRotated(new File("target/test-classes/IMG_0210.JPG")),
+                is(false));
+        assertThat(ImageMetadataUtils.isRotated(new File("target/test-classes/IMG_0480.JPG")),
+                is(false));
+        assertThat(ImageMetadataUtils.isRotated(new File("target/test-classes/killyoukitty.jpg")),
+                is(false));
+        assertThat(ImageMetadataUtils.isRotated(new File("target/test-classes/PA080040.JPG")),
+                is(false));
+        assertThat(ImageMetadataUtils.isRotated(new File("target/test-classes/project.png")),
+                is(false));
+        assertThat(ImageMetadataUtils.isRotated(new File("target/test-classes/ram.jpg")), is(false));
     }
 }
